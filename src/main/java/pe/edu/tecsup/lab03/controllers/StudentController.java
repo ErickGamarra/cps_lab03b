8245ffc21d55f4295053d1b27f6402537b91ba50
Erick Gamarra <erick.gamarra@tecsup.edu.pe>
2026-09-03 15:13:25 -0500

    Cambios en las clases sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,5 +1,7 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+    // Cambios sprint-2
+
     // TO DO
 }
--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,6 @@
 package pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
+    // Cambios sprint-2
+
 }
